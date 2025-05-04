bde5cd5efd28c0a49e5337292f992f272f04025f
niharika singh <98316690+niharika0323@users.noreply.github.com>
2025-05-05 03:38:51 +0530

    Update api.js

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API = axios.create({ baseURL: 'https://parking-finder-app-gaia.onrender.com' });
+const API = axios.create({ baseURL: 'https://parking-finder-app-gaia.onrender.com/api' });
 
 API.interceptors.request.use((req) => {
   const token = localStorage.getItem('token');
